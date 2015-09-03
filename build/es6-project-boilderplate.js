3e989f1d5dcc353f65114236f37a89b9a5846290
mohayonao <mohayonao@gmail.com>
2015-09-04 07:26:40 +0900

    update

--- build/es6-project-boilderplate.js
+++ build/es6-project-boilderplate.js
@@ -1,27 +1,94 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 "use strict";
 
-var _prototypeProperties = function (child, staticProps, instanceProps) { if (staticProps) Object.defineProperties(child, staticProps); if (instanceProps) Object.defineProperties(child.prototype, instanceProps); };
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
 
-var _classCallCheck = function (instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } };
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; desc = parent = getter = undefined; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; continue _function; } } else if ("value" in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var _foo = require("./foo");
+
+var _foo2 = _interopRequireDefault(_foo);
+
+var Bar = (function (_Foo) {
+  _inherits(Bar, _Foo);
+
+  function Bar(name) {
+    _classCallCheck(this, Bar);
+
+    _get(Object.getPrototypeOf(Bar.prototype), "constructor", this).call(this, name);
+  }
+
+  _createClass(Bar, [{
+    key: "hello",
+    value: function hello() {
+      return _get(Object.getPrototypeOf(Bar.prototype), "hello", this).call(this) + "?";
+    }
+  }]);
+
+  return Bar;
+})(_foo2["default"]);
+
+exports["default"] = Bar;
+module.exports = exports["default"];
+},{"./foo":2}],2:[function(require,module,exports){
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 var Foo = (function () {
   function Foo() {
+    var name = arguments.length <= 0 || arguments[0] === undefined ? "" : arguments[0];
+
     _classCallCheck(this, Foo);
+
+    this.name = name;
   }
 
-  _prototypeProperties(Foo, null, {
-    method: {
-      value: function method() {
-        return 10;
-      },
-      writable: true,
-      configurable: true
+  _createClass(Foo, [{
+    key: "hello",
+    value: function hello() {
+      return "hello, " + this.name + "!";
     }
-  });
+  }]);
 
   return Foo;
 })();
 
-module.exports = Foo;
-},{}]},{},[1]);
+exports["default"] = Foo;
+module.exports = exports["default"];
+},{}],3:[function(require,module,exports){
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var _foo = require("./foo");
+
+var _foo2 = _interopRequireDefault(_foo);
+
+var _bar = require("./bar");
+
+var _bar2 = _interopRequireDefault(_bar);
+
+exports["default"] = { Foo: _foo2["default"], Bar: _bar2["default"] };
+module.exports = exports["default"];
+},{"./bar":1,"./foo":2}]},{},[3]);
